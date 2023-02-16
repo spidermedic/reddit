b09cd2393bf2cabbfa61822266aab71002e1efc6
Doug Martin <dougmartinrn@gmail.com>
2023-02-15 19:52:59 -0500

    OAuth error check added

--- scrubby.py
+++ scrubby.py
@@ -14,7 +14,7 @@ def main():
     c  = comments
     p  = posts
     a  = all comments and posts
-    s  = list subreddits with posts or comments
+    ls  = list subreddits with posts or comments
     sc = all comments in a subreddit (opt: subreddit)
     sp = all posts in a subreddit    (opt: subreddit)
         """
@@ -22,7 +22,11 @@ def main():
     post_results = ""
 
     # get oauth information from praw.ini
-    reddit = praw.Reddit("oauth", config_interpolation="basic")
+    try:
+        reddit = praw.Reddit("oauth", config_interpolation="basic")
+        print("Oauth Success")
+    except:
+        logger("Oauth Failed")
 
     # get the command argument and run the appropriate modules
     if len(sys.argv) > 1:
@@ -46,7 +50,7 @@ def main():
                 else:
                     r = input("Which subreddit? r/")
                 comment_results = scrub_subreddit_posts(reddit, r)
-            case "s":
+            case "ls":
                 list_subs(reddit)
             case _:
                 sys.exit(usage)
@@ -54,7 +58,7 @@ def main():
         sys.exit(usage)
 
     # print stats for this run to the screen and add to the log file
-    logger(f"{comment_results}{post_results}")
+    logger(f"{comment_results}\n{post_results}")
 
     sys.exit(0)
 
@@ -63,7 +67,7 @@ def logger(log_data):
     try:
         with open("scrubby.log", "a+") as f:
             print(log_data)
-            f.write(f"{datetime.now().strftime('%m-%d-%Y, %H:%m')}: {log_data}\n")
+            f.write(f"{datetime.now().strftime('%m-%d-%Y, %H:%m')}:\n{log_data}\n\n")
     except:
         print("Unable to write to log file")
 
@@ -101,8 +105,6 @@ def scrub_subreddit_comments(reddit, r):
         # iterate, overwrite and delete any comments found in the subreddit
         if comment.subreddit == r:
             y += 1
-            # comment_date = get_age(comment)["date"]
-            # print(f"{comment_date}, r/{comment.subreddit}:\n{comment.body}\n-----\n")
             comment.edit(body=comment)
             comment.delete()
         x += 1
@@ -130,50 +132,63 @@ def scrub_subreddit_posts(reddit, r):
 
 
 def scrub_comments(reddit):
-    max_posts, y = 0, 0
+    x, y = 0, 0
+    e_error, d_error = 0, 0
 
     # parse the comments, overwrite those older than specified days and delete
     for comment in reddit.user.me().comments.new(limit=None):
 
         # get the date and age of the comment
         a = get_age(comment)
+
         # comment_date = a["date"]
         comment_age = a["age"]
 
         # iterate all of the comments, overwrite and delete any comment over specified days old
-        if max_posts >= 30 or comment_age > 21:
-            comment.edit(body=comment)
-            comment.delete()
+        if x >= 25 or comment_age > 14:
+            try:
+                comment.edit(body=comment)
+            except:
+                e_error += 1
+            try:
+                comment.delete()
+            except:
+                d_error += 1
             y += 1
 
-        # print(f"{comment_age}, r/{comment.subreddit}:\n{comment.body}\n-----\n")
         x += 1
 
     # return the number of comments found and deleted
-    return f"  Comments: {y}/{x}"
+    return f"  Comments: {x}, Deleted: {y}, Edit errors: {e_error}, Delete errors: {d_error}"
 
 
 def scrub_posts(reddit):
     x, y = 0, 0
+    d_error = 0
 
     # get all of the submissions and display the title and date
     for post in reddit.user.me().submissions.new(limit=None):
 
         # get the date of the post
         a = get_age(post)
+
         # post_date = a["date"]
         post_age = a["age"]
 
         # iterate all of the comments, overwrite and delete any comment over specified days old
         if x >= 20 or post_age > 21:
-            post.delete()
+            try:
+                post.delete()
+            except:
+                d_error += 1
+
             y += 1
 
-            print(f"\n{post.date}, r/{post.subreddit}: {post.title}")
+            print(f"\n{a['date']}, r/{post.subreddit}: {post.title}")
         x += 1
 
     # return the number of posts found and deleted
-    return f"  Posts: {y}/{x}"
+    return f"  Posts: {x}, Deleted {y}, Errors: {d_error}"
 
 
 if __name__ == "__main__":
